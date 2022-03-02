c8209222f0908cd0e191624e1a525dc4893e3b86
Sandaru Rasanjana <sandarubro95@gmail.com>
2022-03-02 19:18:45 +0530

    Update index_minimal.py

--- index_minimal.py
+++ index_minimal.py
@@ -1,9 +1,5 @@
 def firstDigitZero(string1):
     return (True if string1[0] == '0' else False)
-def makeIt8Bit(string1):
-    return string1.zfill(8)
-def convert_to_binary_pos(decimal): # F(x) --> Convert Decimal To Binary (Non-Fractional Part)
-    return bin(int(decimal)).replace("0b", "")
 def convert_to_binary_neg(decimal): # F(x) --> Convert Decimal To Binary (Fractional Part)
     binary_string, i = '', 0
     while i != 40:
@@ -31,7 +27,7 @@ while True:
             index_of_dot = user_input.index('.')
         if theres_dot:
             user_input_split = [user_input[0:index_of_dot], ("."+user_input[index_of_dot + 1:])]
-            binary_number = f"{convert_to_binary_pos(float(user_input_split[0]))}.{convert_to_binary_neg(float(user_input_split[1]))}"
+            binary_number = f"{bin(int(int(user_input_split[0]))).replace('0b', '')}.{convert_to_binary_neg(float(user_input_split[1]))}"
             binary_number_without_dot = binary_number.replace(".", "") # =================================== Getting the index of the DOT
             position_of_dot = str(binary_number).index(".") # ============================================== Getting the 2's Exponent
             split_by_dot_0 = binary_number.split('.')[0]
@@ -42,14 +38,11 @@ while True:
             else:
                 pos_first_valued = split_by_dot_1.index('1')
                 power_of_2 = -(((pos_first_valued - position_of_dot)+2))
-            print(f"{sign_binary}.{makeIt8Bit(convert_to_binary_pos(127+power_of_2))}.{binary_number_without_dot[1:24]}")
+            print(f"{sign_binary}.{(bin((127+power_of_2)).replace('0b', '')).zfill(8)}.{binary_number_without_dot[1:24]}")
         else:
-            binary_number = (convert_to_binary_pos(float(user_input)))
+            binary_number = bin(float(str(user_input))).replace("0b", "")
             exp_of_2 = len(binary_number) - 1
             mantissa = binary_number[1:] # Mantissa Normalization
             if len(mantissa) < 25:
-                i = len(mantissa)
-                while i != 25:
-                    mantissa += '0'
-                    i += 1
-            print(f"IEEE 754: {sign_binary}.{makeIt8Bit(convert_to_binary_pos(127+exp_of_2))}.{mantissa[:23]}")
+                mantissa = "{:<08s}".format(mantissa)
+            print(f"IEEE 754: {sign_binary}.{(bin(int(127+exp_of_2)).replace('0b', '')).zfill(8)}.{mantissa[:23]}")
